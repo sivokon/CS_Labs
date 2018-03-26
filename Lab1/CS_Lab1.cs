f1b825d681211583a95087c1f42f25f6a2ff23af
sivokon <32460427+sivokon@users.noreply.github.com>
2018-03-27 01:10:04 +0300

    Lab 1

---
+++ Lab1/CS_Lab1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CS_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceFile = @"file.txt";
+            string fileToWriteIn = @"file(info).txt";
+
+
+            int numOfSymbols = 0;
+            string fileContent = "";
+            Dictionary<char, int> uniqueSymbolsInFile = new Dictionary<char, int>();
+
+            //reading file
+            using (StreamReader reader = new StreamReader(sourceFile, Encoding.UTF8))
+            {
+                fileContent = reader.ReadToEnd();
+            }
+            //fileContent = fileContent.ToLower();
+
+            //counting number of each symbol and whole number of symbols in file
+            foreach (char symbol in fileContent)
+            {
+                if (uniqueSymbolsInFile.ContainsKey(symbol))
+                {
+                    uniqueSymbolsInFile[symbol]++;
+                }
+                else
+                {
+                    uniqueSymbolsInFile.Add(symbol, 1);
+                }
+                numOfSymbols++;
+            }
+
+            //count average entropy and write it in the file
+            using (StreamWriter writer = new StreamWriter(fileToWriteIn, append: false, encoding: Encoding.UTF8))
+            {
+                double H = 0;
+                double p = 0;
+                writer.WriteLine("Number of all symbols:  " + numOfSymbols);
+                writer.WriteLine();
+                foreach (var keyValue in uniqueSymbolsInFile.OrderBy(s => s.Key))
+                {
+                    p = (double)keyValue.Value / numOfSymbols;
+                    //average entropy
+                    H += p * Math.Log(1 / p, 2);
+
+                    //writer.WriteLine(keyValue.Key + "  ->  " + keyValue.Value + ",  Відносна частота появи -  " + p);
+                    if (keyValue.Key == '\n')
+                    {
+                        writer.WriteLine("Відносна частота появи \\n  -  " + p);
+                    }   
+                    else if (keyValue.Key == '\r')
+                    {
+                        writer.WriteLine("Відносна частота появи \\r  -  " + p);
+                    }
+                    else if (keyValue.Key == ' ')
+                    {
+                        writer.WriteLine("Відносна частота появи SPACE  -  " + p);
+                    }
+                    else
+                    {
+                        writer.WriteLine("Відносна частота появи " + keyValue.Key + "  -  " + p);
+                    }
+                }
+                writer.WriteLine();
+                writer.WriteLine("Everage entropy:  " + H);
+                writer.WriteLine();
+                writer.WriteLine("Amount of information:  " + H * numOfSymbols + " bits   =   " + H * numOfSymbols / 8 + " bytes");
+                writer.WriteLine();
+                writer.WriteLine("File size:  " + new FileInfo(sourceFile).Length);
+            }
+        }
+
+    }
+}
---
+++ Lab1/CS_Lab1_BASE64.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+namespace CS_Lab1_BASE64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+            string sourceFile = @"file.txt";
+            string fileToWriteIn = @"file-base64.txt";
+
+
+            StringBuilder base64Result = new StringBuilder();
+            using (BinaryReader reader = new BinaryReader(new FileStream(sourceFile, FileMode.Open, FileAccess.Read)))
+            {
+                byte[] bytes;
+                int[] indices;
+
+                while (true)
+                {
+                    indices = new int[4];
+                    bytes = reader.ReadBytes(3);
+
+                    if (bytes.Length == 0)
+                    {
+                        break;
+                    }
+
+                    for (int i = 0; i < bytes.Length * 8; i++)
+                    {
+                        if ((bytes[i / 8] & (1 << (7 - i % 8))) != 0)
+                        {
+                            indices[i / 6] |= (1 << (5 - i % 6));
+                        }
+                        if (i % 6 == 5 || i == bytes.Length * 8 - 1)
+                        {
+                            base64Result.Append(base64[indices[i / 6]]);
+                        }
+                    }
+
+                    if (bytes.Length == 2)
+                    {
+                        base64Result.Append("=");
+                    }
+                    else if (bytes.Length == 1)
+                    {
+                        base64Result.Append("==");
+                    }
+                }
+
+            }
+            //Console.WriteLine(base64Result);
+
+            string base64ResultWithBuiltInFunc = Convert.ToBase64String(File.ReadAllBytes(sourceFile));
+
+            //if our result is similar with built-in function - write it to the file
+            if (base64Result.ToString() == base64ResultWithBuiltInFunc)
+            {
+                using (StreamWriter writer = new StreamWriter(fileToWriteIn, append: false, encoding: Encoding.UTF8))
+                {
+                    writer.WriteLine(base64Result);
+                }
+            }
+
+
+            //Console.ReadLine();
+        }
+    }
+}
